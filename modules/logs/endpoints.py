66f5463163565d533e1545d625cb4c5e9bbe58ff
liucongpei <liucongpei@126.com>
2019-11-01 18:41:06 +0800

    get_kettle_action_log

--- modules/logs/endpoints.py
+++ modules/logs/endpoints.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import sqlite3
 from flask import Blueprint, request, send_from_directory, json
 from flask_classy import FlaskView, route
 from modules import cbpi
@@ -68,6 +69,29 @@ class LogView(FlaskView):
     def convert_chart_data_to_json(self, chart_data):
         return {"name": chart_data["name"], "data": self.read_log_as_json(chart_data["data_type"], chart_data["data_id"])}
 
+    def get_kettle_action_log(self, kid):
+        sql = "select strftime('%s',nTime) as nTime,nTarget_Tem,nCur_Tem,nStatus from tbksensor_log where nKettleID=%s;"%(kid)
+        conn = sqlite3.connect("sensor_log.db")
+        cursor = conn.execute(sql)
+        array1 = []
+        array2 = []
+        array3 = []
+        for row in cursor:
+            array1.append([int(row[0]),float(row[1])])
+            array2.append([int(row[0]), float(row[2])])
+            array3.append([int(row[0]), float(row[3])])
+
+        conn.close()
+        a1 = {"name": "Target Temp", "data":array1}
+        a2 = {"name": "Cur Temp", "data": array2}
+        a3 = {"name": "Act Status", "data": array3}
+
+        ret = []
+        ret.append(a1)
+        ret.append(a2)
+        ret.append(a3)
+        return ret
+
     @route('/<t>/<int:id>', methods=["POST"])
     def get_logs_as_json(self, t, id):
         print "get logs as json %s %s"%(t,id)
@@ -85,9 +109,10 @@ class LogView(FlaskView):
             fermenter = cbpi.cache.get("fermenter").get(id)
             result = map(self.convert_chart_data_to_json, cbpi.get_fermentation_controller(fermenter.logic).get("class").chart(fermenter))
 
-        if t=="act_sensor":
-            kettle = cbpi.cache.get("kettle").get(id)
-            result = map(self.convert_chart_data_to_json, cbpi.get_controller(kettle.logic).get("class").chart(kettle))
+        if t == "act_sensor":
+            result = self.get_kettle_action_log(id)
+            #kettle = cbpi.cache.get("kettle").get(id)
+            #result = map(self.convert_chart_data_to_json, cbpi.get_controller(kettle.logic).get("class").chart(kettle))
             print result
         return json.dumps(result)
 
